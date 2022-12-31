de2752ce562cfbe8f2ad31b85d2ded4b755c629b
Arzu Caner <arzuguneycaner@gmail.com>
2022-12-31 23:45:32 +0000

    Add the box

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ let inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click",function() {
     console.log("Button clicked from addEventListener")
 })
+
+let box = document.getElementById("box")
+
+box.addEventListener("click", function(){
+    console.log("I want to open the box!")
+})
